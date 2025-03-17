3bcb0c9b9df37c56e25388795e99c197f8685138
Love <lovekumar.200442@gmail.com>
2025-03-17 15:27:20 +0530

    refresh to update the ui

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,21 +1,26 @@
-import React, { useEffect,useState } from 'react'
+import React,{useState,useEffect} from "react";
 
 const TransactionList = () => {
- 
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const storedTransaction =
+      JSON.parse(localStorage.getItem("transactions")) || [];
+    setTransactions(storedTransaction);
+  }, []);
+
   return (
     <div>
-        <h3>History</h3>
-        <ul id="list" className="list">
-            {
-              transactions.map((t)=>(
-                <li key={t.id}>
-                  {t.text} <span>{t.amount}</span>
-                </li>
-              ))
-            }
-        </ul>
+      <h3>History</h3>
+      <ul id="list" className="list">
+        {transactions.map((t) => (
+          <li key={t.id}>
+            {t.text} <span>{t.amount}</span>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default TransactionList
+export default TransactionList;
